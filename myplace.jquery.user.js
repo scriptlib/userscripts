79dacf2755962cba171bfbe2eee4a1c8a90c9a7f
Eotect Nahn <eotect@gmail.com>
2013-09-23 17:45:33 +0800

    Create myplace.jquery.user.js

---
+++ myplace.jquery.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name           myplace.jquery
+// @namespace      eotect@myplace
+// @description    $myPlace.jQuery
+// @include        *
+// @require			http://code.jquery.com/jquery-1.10.2.min.js
+// @require			http://code.jquery.com/ui/1.10.2/jquery-ui.min.js
+// ==/UserScript==
+
+$myPlace=unsafeWindow.$myPlace;
+if(!$myPlace) {
+  $myPlace = {};
+}
+unsafeWindow.$myPlace = $myPlace;
+
+(function(_){
+if($) {
+	var csslink = document.createElement('link');
+	csslink.href = "http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css";
+	csslink.setAttribute('rel',"stylesheet");
+	csslink.type ="text/css";
+	$('head').append(csslink);
+	_.jQuery = $;
+	_.$ = $;
+}
+})($myPlace);
