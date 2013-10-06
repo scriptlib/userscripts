6eb396e0aa1b1b62ebce375ff83ad8a7601e83c2
Eotect Nahn <eotect@gmail.com>
2013-10-06 23:23:05 +0800

    Quick links for baidu.yun
    	Add file: myplace.baidu.yun.panel.user.js

---
+++ myplace.baidu.yun.panel.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name       myplace.baidu.yun.panel
+// @namespace  eotect@myplace
+// @version    0.1
+// @description  enter something useful
+// @include     http://pan.baidu.com/pcloud/friendpage*
+// @include     http://pan.baidu.com/share/*
+// @copyright  2012+, eotect
+// @grant none
+// ==/UserScript==
+
+(function(){
+    if(typeof unsafeWindow == 'undefined') {
+    	var unsafeWindow = window;
+        window.unsafeWindow = window;
+    }
+    if(typeof $myPlace == 'undefined') {
+        var $myPlace = unsafeWindow.$myPlace;
+    }
+    var $ = $myPlace.jQuery;   
+    function ukFromUrl(href) {
+        var r = href.match(/uk=(\d+)/);
+        if(r) {
+            return r[1];
+        }
+        return 0;
+    }
+    var href = document.location.href;
+    var pageUK = ukFromUrl(href);
+    var followsPage = "http://pan.baidu.com/pcloud/friendpage?type=follow&uk=" + pageUK;
+    var fansPage = "http://pan.baidu.com/pcloud/friendpage?type=fans&uk=" + pageUK;
+    if($('#share_nofound_des').length || href.match(/\/pcloud\/friendpage/)) {		
+        	$myPlace.panel.add($('<a href="/share/home?uk=' + pageUK + '">' + pageUK + '</a>')[0],1); 
+	}
+	$myPlace.panel.addSpace();
+    $myPlace.panel.add($('<a href="' + followsPage + '">Follows</a>')[0],1);
+    $myPlace.panel.add($('<a href="' + fansPage + '">Fans</a>')[0]);
+    $myPlace.panel.show();
+    
+})();
