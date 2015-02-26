e79fa191a8409b36514c286a3686d3426b809744
Eotect Nahn <eotect@gmail.com>
2015-02-26 20:53:22 +0800

    Add file: myplace.baidu.yun.wap.user.js

---
+++ myplace.baidu.yun.wap.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name        myplace.baidu.yun.wap
+// @namespace   eotect@myplace
+// @description myplace.baidu.yun.wap
+// @include     http://pan.baidu.com/wap/*
+// @include     http://yun.baidu.com/wap/*
+// @version     1.02
+// @grant       none
+// ==/UserScript==
+
+(function(){
+    if(typeof unsafeWindow == 'undefined') {
+    	var unsafeWindow = window;
+        window.unsafeWindow = window;
+    }
+    if(typeof $myPlace == 'undefined') {
+        var $myPlace = unsafeWindow.$myPlace;
+    }
+	var $ = $myPlace.jQuery;   
+	var href = document.location.href;
+	if(href) {
+		var m = href.match(/(^https?:\/\/(?:pan|yun)\.baidu\.com)\/(?:wap\/share|wap)\/(.*)$/);
+		if(m) {		
+			$myPlace.panel.addLink(m[1] + "/share/" + m[2],'Desktop Version');
+		}
+	}
+})();
