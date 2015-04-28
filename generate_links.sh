b9853f7545d604fe004cc1f0a60232b0e7ad243b
Eotect Nahn <eotect@gmail.com>
2015-04-29 01:54:03 +0800

    Update files:generate_links.sh,links.html

--- generate_links.sh
+++ generate_links.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-dist="http://userscripts.webscrap.googlecode.com/git"
-echo "<ol>Links" | tee links.html
+dist="http://bitbucket.org/eote/userscripts/raw/master"
+echo "<html><body><ol>Links" | tee links.html
 ls -1 *.js *.css | xargs -l -n1 -i echo "<li><a href=\"$dist/{}\">{}</a></li>" | tee -a links.html
-echo "</ol>" | tee -a links.html
+echo "</ol></body></html>" | tee -a links.html
 
 
 
